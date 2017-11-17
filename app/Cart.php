c5ddcd86b7ff0f5bdf972dc0dbc7da43b556f561
AndrewKuktenko <andriy.kuktenko@gmail.com>
2017-11-17 15:29:04 +0200

    Добавлена верстка заказов, возможность удалять товары из корзины

--- app/Cart.php
+++ app/Cart.php
@@ -18,7 +18,11 @@ class Cart
     }
 
     public function add($item, $id) {
-        $storedItem = ['qty' => 0, 'price' => $item->price, 'item' => $item];
+        $storedItem = [
+            'qty' => 0,
+            'price' => $item->price,
+            'item' => $item,
+        ];
         if ($this->items) {
             if (array_key_exists($id, $this->items)) {
                 $storedItem = $this->items[$id];
@@ -29,5 +33,22 @@ class Cart
         $this->items[$id] = $storedItem;
         $this->totalQty++;
         $this->totalPrice += $item->price;
+    }
+
+    public function reduceByOne($id){
+        $this->items[$id]['qty']--;
+        $this->items[$id]['price'] -= $this->items[$id]['item']['price'];
+        $this->totalQty--;
+        $this->totalPrice -= $this->items[$id]['item']['price'];
+
+        if ($this->items[$id]['qty'] <= 0) {
+            unset($this->items[$id]);
+        }
+    }
+
+    public function removeItem($id) {
+        $this->totalQty -= $this->items[$id]['qty'];
+        $this->totalPrice -= $this->items[$id]['price'];
+        unset($this->items[$id]);
     }
 }
